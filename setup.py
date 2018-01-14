c2ffdb08cfba5889d2b80f233f293d33710fefc0
Andrew Carter <andrew.carter@smarterdm.com>
2018-01-14 20:41:27 +0000

    Added manifest

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pascal-voc-writer',
-    version='0.1.2',
+    version='0.1.3',
 
     description='Create image annotation XML files in the PASCAL VOC format',
     long_description=long_description,
